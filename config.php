f8d370f3c221826ee6eacf6c486c7f9097dd259f
Fahad Akbar <fahadakbar24@gmail.com>
2023-02-10 10:06:31 +0500

    Added idea files

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "fb_api_ver" => "v16.0",
+    "fb_app_id" => "639879514575287",
+    "fb_app_secret" => "33b9101211b1d54cf1743529f3fe8a52",
+    "fb_permissions" => "pages_show_list,page_events,pages_read_engagement,pages_manage_metadata,pages_read_user_content,pages_manage_posts,pages_manage_engagement",
+];
