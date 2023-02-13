f6f8146962f30e48559eecfa34281040e16c80eb
Fahad Akbar <fahadakbar24@gmail.com>
2023-02-14 02:45:44 +0500

    added config.php sample

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    "domain" => "http://localhost/",
-    "fb_api_ver" => "v16.0",
-    "fb_app_id" => "639879514575287",
-    "fb_app_secret" => "33b9101211b1d54cf1743529f3fe8a52",
-    "fb_permissions" => "pages_show_list,page_events,pages_read_engagement,pages_manage_metadata,pages_read_user_content,pages_manage_posts,pages_manage_engagement",
-];
